4148913525437aacbf3ea3e8d759dde83198abf7
hadizzo <masimo.hadizzo@gmail.com>
2023-06-01 22:37:22 +0330

    api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect } from "react";
+import { assertExpressionStatement } from "@babel/types";
+import axios from "axios";
+import { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [data, setdata] = useState();
+  useEffect(() => {
+    axios
+      .get("https://fakestoreapi.com/products/1")
+      .then((result) => setdata(result.data));
+  }, []);
+
+  return <div className="App">{data}</div>;
 }
 
 export default App;
