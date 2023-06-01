5ea713706c6ca6248cb188496176235d0318d5a9
hadizzo <masimo.hadizzo@gmail.com>
2023-06-01 20:17:37 +0330

    api called successfully

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { React, useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
+  const baseURL = "https://jsonplaceholder.typicode.com/posts/1";
+  const [post, setPost] = useState(null);
+
+  useEffect(() => {
+    axios.get(baseURL).then((response) => {
+      setPost(response.data);
+    });
+  }, []);
+
+  if (!post) return null;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{post.title}</h1>
+      <p>{post.body}</p>
     </div>
   );
 }
